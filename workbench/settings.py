02d98f306d84972929337c8cc022f051b1d7ab78
Tabik Karbayev <tabigatkarbayev@icloud.com>
2023-10-14 00:08:08 +0600

    oct_changes

--- workbench/settings.py
+++ workbench/settings.py
@@ -86,15 +86,23 @@ WSGI_APPLICATION = "workbench.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+#
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": 'workbench_db',
+#         "USER": 'admin',
+#         "PASSWORD": 'admin',
+#         "HOST": 'localhost',
+#         "PORT": '',
+#     }
+# }
+
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": 'workbench_db',
-        "USER": 'admin',
-        "PASSWORD": 'admin',
-        "HOST": 'localhost',
-        "PORT": '',
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
