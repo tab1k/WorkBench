ca1ac8d7640f9f20f246c4a1b3f1c4e75170fa35
Карбаев Табигат <tabigat@MacBook-Pro-Karbaev.local>
2024-01-28 14:58:56 +0600

    changes

--- blog/models.py
+++ blog/models.py
@@ -2,17 +2,33 @@ from django.db import models
 
 
 class Post(models.Model):
+
+    HASHTAG = (
+        ('startups', 'Стартапы'),
+        ('programming', 'Программирование'),
+        ('digital', 'Цифровизация'),
+        ('strategy', 'IT стратегия'),
+        ('physics', 'Физика'),
+        ('math', 'Математика'),
+        ('business', 'IT Бизнес'),
+        ('technology', 'Технологии'),
+        ('lifehacks', 'Лайфхаки'),
+    )
+
     title = models.CharField(max_length=255, blank=False)
     author = models.CharField(max_length=255, blank=False)
     author_url = models.URLField(blank=True, null=True)
     image = models.ImageField(upload_to='posts/', blank=False)
     video = models.URLField(blank=True, null=True)
     body = models.TextField(blank=False)
-    hashtag = models.CharField(max_length=255, blank=False)
+    hashtag = models.CharField(max_length=255, choices=HASHTAG, default='programming',blank=False)
     date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
+
+    def get_hashtag_display(self):
+        return dict(Post.HASHTAG)[self.hashtag]
 
     class Meta:
         ordering = ['-date']
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,6 @@
+from django.contrib.postgres.search import SearchVector, SearchQuery
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+from django.db.models import Q
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 
@@ -11,7 +14,39 @@ class BlogListView(ListView):
     paginate_by = 10
 
     def get_queryset(self):
-        return Post.objects.filter().order_by('-date')
+        queryset = Post.objects.all().order_by('-date')
+
+        # Filter by hashtag
+        hashtag_filter = self.request.GET.get('hashtag', '')
+        if hashtag_filter:
+            queryset = queryset.filter(hashtag=hashtag_filter)
+
+        # Search
+        search_query = self.request.GET.get('search', '')
+        if search_query:
+            search_condition = Q()
+            for field in ['title', 'author', 'body', 'hashtag']:
+                search_condition |= Q(**{f'{field}__icontains': search_query})
+
+            queryset = queryset.filter(search_condition)
+
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        # Pagination
+        paginator = Paginator(context['posts'], self.paginate_by)
+        page = self.request.GET.get('page')
+
+        try:
+            context['posts'] = paginator.page(page)
+        except PageNotAnInteger:
+            context['posts'] = paginator.page(1)
+        except EmptyPage:
+            context['posts'] = paginator.page(paginator.num_pages)
+
+        return context
 
 
 class BlogDetailView(DetailView):
